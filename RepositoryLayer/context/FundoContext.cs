dba9217689721322c84768778c146402c1d783fb
akhilkumar16 <akhilkumarchandragiri@gmail.com>
2022-02-22 14:19:46 +0530

    [Akhil] created db connecction and tables

---
+++ RepositoryLayer/context/FundoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.context
+{
+    public class FundoContext : DbContext
+    {
+        public FundoContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        public DbSet<User> UserTables { get; set; }
+    }
+}
---
+++ RepositoryLayer/entities/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Web.Services3.Security.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
