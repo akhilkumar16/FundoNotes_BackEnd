daf4e5f1f89d7b2ccfaf18913a1306d51bec214a
akhilkumar16 <akhilkumarchandragiri@gmail.com>
2022-02-23 10:08:27 +0530

    [Akhil] updated with regex

--- CommonLayer/models/UserRegmodel.cs
+++ CommonLayer/models/UserRegmodel.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CommonLayer.models
 {
     public class UserRegmodel
     {
+        [Required]
+        [RegularExpression(@"^[A-Z]{1}[a-z]{3}$")]
         public string FirstName { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-Z]{1}[a-z]{3}$")]
         public string LastName { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-c]{3}[.+-_]{0,1}[x-z]{3}@[a-z]{2}[.+-]{0,1}[a-z]{2}[.+-]{0,1}[a-z]{2}$")]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"^((?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_.])(?=.{8,}))")]
         public string Password { get; set; }
     }
 }
--- RepositoryLayer/entities/User.cs
+++ RepositoryLayer/entities/User.cs
@@ -9,12 +9,19 @@ namespace RepositoryLayer.entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-Z]{1}[a-z]{3}$")]
         public string FristName { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-Z]{1}[a-z]{3}$")]
         public string LastName { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-c]{3}[.+-_]{0,1}[x-z]{3}@[a-z]{2}[.+-]{0,1}[a-z]{2}[.+-]{0,1}[a-z]{2}$")]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"^((?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_.])(?=.{8,}))")]
         public string Password { get; set; }
-        //public string PhoneNumber { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? ModifiedAt { get; set; }
-        public string Email { get; set; }
     }
 }
